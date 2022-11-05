574f2706a8e198b37d40030f3ac0f486db2f4ef0
veysel basbaydar <vgbasbaydar@gmail.com>
2022-11-05 13:53:05 -0700

    about component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 
 function App() {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="a">
+      <div className="a-left">Left</div>
+      <div className="a-right">Right</div>
+    </div>
+  );
+}
