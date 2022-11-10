d0ed3ab10ee30d38e60974c6a334e9970f5ddfd0
veysel basbaydar <vgbasbaydar@gmail.com>
2022-11-09 20:14:06 -0800

    working on contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,27 +2,42 @@ import React from "react";
 import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
-import Adress from "../../img/address.png";
+import Address from "../../img/address.png";
 
 export default function Contact() {
   return (
     <div className="c">
-      <div className="c-bg">
-        <div className="c-wrapper">
-          <div className="c-left">
-            <h1 className="c-title">Let discuss your project</h1>
-            <div className="c-info">
-              <div className="c-info-item">
-                <img src={Phone} alt="" className="c-icon" />
-                +1 650 4850316
-                <img src={Email} alt="" className="c-icon" />
-                vgbasbaydar@gmail.com
-                <img src={Adress} alt="" className="c-icon" />
-                820 Clinton St, Redwood Cit, CA , 94061
-              </div>
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +1 1234 556 75
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              contact@lama.dev
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              245 King Street, Touterie Victoria 8520 Australia
             </div>
           </div>
-          <div className="c-right">right</div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+          </form>
         </div>
       </div>
     </div>
